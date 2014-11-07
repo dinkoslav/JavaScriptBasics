2caf11921fbdce9e963976da29e0f080639132d0
dinkoslav <dinkoslav@gmail.com>
2014-11-07 19:38:06 +0200

    Create Exam1.js

---
+++ 02.JavaScript-Syntax/Exam1.js
@@ -0,0 +1,31 @@
+function printTableOfNumbers(input){
+	var start = input[0];
+	var end = input[1];
+	console.log('<table>');
+	console.log('<tr><th>Num</th><th>Square</th><th>Fib</th></tr>');
+	
+	for( currNum=start ; currNum<=end ; currNum++){
+		console.log('<tr><td>' + currNum + '</td><td>' + currNum*currNum + '</td><td>' + isFib(currNum) + '</td></tr>');
+	}
+	function isFib(currNum){
+		var fib1 = 1;
+		var fib2 = 1;
+		var fib3 = 0;
+		if(currNum == 1){
+			return 'yes';
+		}
+		else{
+			while(fib3<currNum){
+				fib3 = fib1 + fib2;
+				if(currNum == fib3){
+					return 'yes';
+				}
+				fib1 = fib2;
+				fib2 = fib3;
+			}
+			return 'no';
+		}
+	}
+	
+	console.log('</table>');
+}
